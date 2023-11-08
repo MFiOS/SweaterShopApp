d5f56b11c1b5b4dab6fc6876f01834f59ada1ca6
ichangyou <564448895@qq.com>
2023-11-08 21:31:51 +0800

    init project

---
+++ SweaterShopApp/SweaterShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopAppApp.swift
+//  SweaterShopApp
+//
+//  Created by chang on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
